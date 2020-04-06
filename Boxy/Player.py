ee1b7104e1127aa7e32b6d4f7b73f11361a910e8
sdillavou <sam.dillavou@gmail.com>
2020-04-06 18:06:25 -0400

    Attack functionality added, plus red hitboxes for debugging

--- Boxy/Player.py
+++ Boxy/Player.py
@@ -15,9 +15,13 @@ crawling_speed = 1.5*S
 running_speed = 4.5*S
 accel,decel = 0.15*S,.15*S
 slide_duration = 10
+attack_duration = 15
 slide_speed = 7.5*S
+crouch_fraction = 0.5
 character_color = (255,0,255)
 sliding_color = (200,0,200)
+attack_fraction = 1.2
+slide_fraction = 1.2
 
 #z_proj = [0.2,0.1,-1] # shift of 1 in z corresponds to this much shift in projected view
 
@@ -37,8 +41,24 @@ class Player(Body):
         self.attacking = 0
         self.shapes.append(Shape(self.self_shape(),(255,0,255),line_color = None,line_width = None)) # add visible shape for box
         self.size+= 1.0
-    
-    
+        
+        # hitbox for attack
+        self.attack_box = Body([0,0],player_size*np.array([attack_fraction,1.0],dtype='float'),solid=False)
+        self.attack_box.shapes.append(Shape(self.attack_box.self_shape(),color = (255,0,0),line_color = None)) # add outline
+        # hitbox for slide
+        self.slide_box = Body([0,0],player_size*np.array([slide_fraction,crouch_fraction],dtype='float'),solid=False)
+        self.slide_box.shapes.append(Shape(self.slide_box.self_shape(),color = (255,0,0),line_color = None)) # add outline
+        
+    # return relevant hit box (or None if no hit box being used)
+    def hit_box(self):
+        if self.sliding>0:
+            return self.slide_box
+        elif self.attacking>0:
+            return self.attack_box
+        else:
+            return None
+        
+        
     def draw(self,canvas,zero = np.array([0,0])):
         
         if self.sliding>0:
@@ -48,11 +68,20 @@ class Player(Body):
         for s in self.shapes:
             s.color = color
         
+        if isinstance(self.hit_box(),Body): # draw relevant hit box
+            self.hit_box().draw(canvas,zero)
+        
         super().draw(canvas,zero)
         
+       
+    def move(self):
+        super().move()
+        self.slide_box.pos = self.pos*1.0
+        self.attack_box.pos = self.pos*1.0
+
     
     # gravity, running, sliding, attacking, belly-flopping, gravity, all in one go
-    def evolve(self,run_key,crouch_key,jump_key):
+    def evolve(self,run_key,crouch_key,jump_key,attack_key):
         
         # add gravity's effects
         self.vel[1] = min(max_fall_speed,self.vel[1]+G)
@@ -62,7 +91,7 @@ class Player(Body):
         decelerating = (run_key == 0 or run_key*self.vel[0] <0)
 
         
-        if not airborne and jump_key and self.attacking<=0: #jump if on the ground and not attacking
+        if not airborne and jump_key and self.attacking<=0: #jump if on the ground and not attacking (can jump out of slide)
             self.vel[1] = (-jump_strength*(1+ crouch_bonus*(self.crouching>0)))
             self.is_off() # no longer standing on an object
             self.sliding = 0 # cancels slides 
@@ -76,28 +105,36 @@ class Player(Body):
             
         else: # if not sliding, some control  
             
+            # deal with attacking
+            
+            if attack_key and not (self.attacking>0):
+                self.attacking = attack_duration
+                
+            elif self.attacking>0:
+                self.attacking -= 1   
+            
             # deal with slide/crouch key
             if self.crouching:
-                if not crouch_key or airborne: # if key released or in the air, uncrouch
+                if not crouch_key or airborne or (self.attacking>0): # if key released or in the air, or attacking, uncrouch
                     self.pos[1] -= np.matmul(self.transform,self.size)[1]
                    # self.transform[0][:] *= 0.8
-                    self.transform[1][:] *= 2.0
+                    self.transform[1][:] *= (1/crouch_fraction)
                     self.crouching = False
             else: # if uncrouched
-                if not self.attacking and crouch_key and not airborne: # down key pressed and on the ground and not attacking
+                if not (self.attacking>0) and crouch_key and not airborne: # down key pressed and on the ground and not attacking
 
                     # crouch/slide position
                    # self.transform[0][:] *= 1.25
-                    self.transform[1][:] *= 0.5
+                    self.transform[1][:] *= crouch_fraction
                     self.pos[1] += np.matmul(self.transform,self.size)[1]
                     self.crouching = True
                     
                     if run_key: # if already running, slide!
                         self.sliding = slide_duration # 10 frames of sliding!
                         self.vel[0] = slide_speed*run_key
-            
-        if self.sliding <=0:           
-            if airborne or self.attacking>0: # if airborne or attacking, control is weak, cannot crouch or slide
+                        return # no walking/running if sliding
+                    
+            if airborne or (self.attacking>0): # if airborne or attacking, control is weak, cannot crouch or slide
                 if decelerating:
                     self.vel[0] -= math.copysign(min(decel,abs(self.vel[0])),self.vel[0]) # decelerate but not past 0
                 else: # accelerating
@@ -115,5 +152,5 @@ class Player(Body):
                     self.vel[0] =  top_speed*run_key # start walking/crawling at  speed
         
             
-
+        
    
