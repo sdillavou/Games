Size scaling now global and bug fixes
changing S in constants.py now scales window size and status size as well. This makes game shrinkable, and allows faster/smoother frame rates.

also fixed the hovering after slide-jump-hitting underside of a floating box bug, and player.direction now corresponds to arrow keys, not velocity.

Boxy/Constants.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-S=1.4 # global scaling
+S=1.2 # global scaling
 #S = 0.5
 S = float(S) # doubly sure... heh
 
@@ -8,7 +8,7 @@ G = 0.9*S # strength of gravity
 box_size = 30*S
 
 character_color = (31, 61, 12)
-display_size = (1000,600)
+display_size = (int(720*S),int(405*S)) # 16:9 ratio
 floor = display_size[1]*5.0/6.0
 
 
@@ -16,3 +16,5 @@ protector_color = (60,60,255)
 protector_line_color = (255,255,255)
 protector_size = np.array([box_size*0.5,box_size*0.5],dtype='float')
 eye_color = (200,200,200)
+
+